f26b1edc47357a19249c8edd4217428588fa517d
Jay Dobies <jason.dobies@redhat.com>
2016-10-04 16:18:58 -0400

    Dumb change to change the env port to an int

--- web.py
+++ web.py
@@ -2,7 +2,7 @@ import http.server
 import os
 
 
-PORT = os.environ.get('SERVE_PORT', None) or 8000
+PORT = int(os.environ.get('SERVE_PORT', None)) or 8000
 
 
 class SimpleHandler(http.server.BaseHTTPRequestHandler):
